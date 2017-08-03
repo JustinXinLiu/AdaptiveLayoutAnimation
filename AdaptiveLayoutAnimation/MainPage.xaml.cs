8244632f5233b9e914f2d1196d71e12dc63f4621
Justin Liu <justin.xin.liu@gmail.com>
2017-08-04 06:43:22 +1000

    Initial.

--- AdaptiveLayoutAnimation/MainPage.xaml.cs
+++ AdaptiveLayoutAnimation/MainPage.xaml.cs
@@ -1,30 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using Windows.UI.Xaml.Controls;
+using Continuity;
+using Continuity.Extensions;
 
 namespace AdaptiveLayoutAnimation
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            LeftText.EnableImplicitAnimation(VisualPropertyType.Offset, 400);
+            RightText.EnableImplicitAnimation(VisualPropertyType.Offset, 400);
+
+            var compositor = this.Visual().Compositor;
+            //var leftBackgroundVisual = compositor.
+
+            LeftGrid.SizeChanged += (s, e) => { };
+            RightGrid.SizeChanged += (s, e) => { };
         }
     }
 }
